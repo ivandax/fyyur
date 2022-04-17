a4d20822dbf1ba97de806e5cce4dc9d5e0aa8d48
ivan.dario <ivan.dario@nexiona.com>
2022-04-17 16:24:55 +0200

    Implement artist and venue search

--- config.py
+++ config.py
@@ -8,7 +8,5 @@ DEBUG = True
 
 # Connect to the database
 
-
-# TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
